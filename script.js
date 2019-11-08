ca982825f91380a971be4264e37096c83fdc0c16
valdimar02 <valdimar.brimir@nme.is>
2019-11-08 13:43:23 +0000

    Búinn

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+let greeting = "Howdy";
+let name = "Molly";
+let message = "please check your order";
+let welcome = greeting + name + message;
+
+let sign = "Montague House"; // => 14 á lengdina
+let tiles = sign.length; // lengdin á sign
+let subTotal = tiles * 5;
+let shipping = 7;
+let grandTotal = subTotal + shipping;
+
+let el = document.getElementById("greeting");
+let elSign = document.getElementById("userSign");
+let elTiles = document.getElementById("tiles");
+let elSubTotal = document.getElementById("subTotal");
+let elShipping = document.getElementById("shipping");
+let elGrandTotal = document.getElementById("grandTotal");
+
+
+el.textContent = welcome;
+
+elSign.textContent = sign;
+
+elTiles.textContent = tiles;
+
+elSubTotal.textContent = "$" + subTotal;
+
+elShipping.textContent = "$" + shipping;
+
+elGrandTotal.textContent = "$" + grandTotal;
+
+
+
+
+
 
